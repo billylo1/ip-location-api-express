782d3f65e7057388d4ee684b41e4bb6c462209c5
Billy Lo <billylo1@gmail.com>
2024-12-03 08:20:17 -0500

    add fallback to get missing city

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.get('/api/location/:ip', async (req, res) => {
     const ip = req.params.ip;
     try {
         const location = await lookup(ip);
+        if (location.city == undefined) {
+            location.city = await getCity(location.latitude, location.longitude);
+        }
         res.json(location);
     } catch (error) {
         console.error('Error fetching location:', error);
@@ -34,3 +37,22 @@ app.listen(PORT, async () => {
     console.log(`Server is running on http://0.0.0.0:${PORT}`);
     await reload({fields: 'latitude,longitude,country,country_name,region1_name,city'})
 });
+
+const GOOGLE_MAPS_API_KEY="AIzaSyCAlshhT1DTtzc_dNYRlxGvl9c_NTJOZuU";
+
+async function getCity(lat, lng) {
+
+    // use google map reverse geocoding api
+    try {
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_MAPS_API_KEY}&&result_type=locality`;
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(JSON.stringify(data, null, 2));
+        const city = data?.results[0]?.address_components[0]?.long_name;
+        // console.log('city', city);
+        return Promise.resolve(city);
+    } catch (error) {
+        console.error('Error fetching location:', error);
+        return Promise.resolve("No city found");
+    }
+}
